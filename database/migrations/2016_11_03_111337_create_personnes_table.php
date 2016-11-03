cd47d72652385f825bd93c91dede7ab06ffc6243
Anthony Lozano <anthony.lozano@anthozano.fr>
2016-11-03 21:08:52 +0100

    Trying to understand Laravel migrations

---
+++ database/migrations/2016_11_03_111337_create_personnes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonnesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->char('sexe', 1);
+            $table->string('naissance');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnes');
+    }
+}
