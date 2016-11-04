faffd8984a9a202fda73f6479b7b3a2aabdc6c06
Anthony Lozano <anthony.lozano@anthozano.fr>
2016-11-04 01:25:49 +0100

    Added models

---
+++ app/Lieu.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lieu extends Model
+{
+    public $timestamps = false;
+
+}
---
+++ app/Marie.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Marie extends Model
+{
+    public $timestamps = false;
+
+}
