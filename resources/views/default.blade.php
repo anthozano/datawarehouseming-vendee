7204ba1d008bed0f3f655625fdec243c27b2ee54
Anthony Lozano <anthony.lozano@anthozano.fr>
2016-11-11 01:56:55 +0100

    Whoops :)

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -60,5 +60,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.bundle.min.js"></script>
 
+@yield('script')
+
 </body>
 </html>
